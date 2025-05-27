6b83256a31cbf5d09ae92ad60f5e5998407de908
dljardim <dljardim@outlook.com>
2025-05-27 11:10:34 -0500

    Initial Commit

---
+++ InterviewEx/InterviewExApp.swift
@@ -0,0 +1,17 @@
+//
+//  InterviewExApp.swift
+//  InterviewEx
+//
+//  Created by Damian Jardim on 5/27/25.
+//
+
+import SwiftUI
+
+@main
+struct InterviewExApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
